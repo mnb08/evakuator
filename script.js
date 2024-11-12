c0d8437027975e416e2e0e9d21e900fa6bb767f5
Amanbai <mnbsaadbv@gmail.com>
2024-11-13 00:03:02 +0600

    hi

---
+++ script.js
@@ -0,0 +1,16 @@
+let headerLink = document.querySelector(".salom");
+console.log(headerLink);
+
+// headerLink.addEventListener("click", function () {
+// console.log("Hello");
+// });
+
+// let footer = document.querySelector(".site-footer");
+// let clone = footer.cloneNode(footer);
+// console.dir(clone.hasChildNodes(footer));
+
+// console.log(window.history);
+
+// window.moveTo = function () {
+// 	alert("Hello! I am an alert box!!");
+// };
